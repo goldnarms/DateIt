8845212ae7342c71e59f7ad0dfd04d5aaba43b8d
Arnstein Johansen <arnstej@gmail.com>
2013-04-07 21:30:50 +0200

    First commit

---
+++ DateIt.PCL/Interfaces/IBarcodeScanner.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DateIt.PCL.Interfaces
+{
+    public interface IBarcodeScanner
+    {
+        public int Scan(byte[] image);
+    }
+}
---
+++ DateIt.PCL/Services/BarcodeScanner.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DateIt.PCL.Interfaces;
+
+namespace DateIt.PCL.Services
+{
+    public class BarcodeScanner : IBarcodeScanner
+    {
+        public int Scan(byte[] image)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
